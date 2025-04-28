da46d83441a875673b8c39a74d5226761c337fa4
Inov Afani <inoveka@gmail.com>
2025-04-28 12:12:14 +0700

    Update the porto

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 use Laravel\Socialite\Facades\Socialite;
 
 class authController extends Controller
@@ -24,19 +26,31 @@ class authController extends Controller
         $id = $user->id;
         $email = $user->email;
         $name = $user->name;
+        $avatar = $user->avatar;
 
         $cek = User::where('email', $email)->count();
         if($cek > 0){
+            $avatar_file = $id . ".jpg";
+            $fileContent = file_get_contents($avatar);
+            File::put(public_path("admin/images/faces/$avatar_file"), $fileContent);
             $user = User::updateOrCreate(
                 ['email' => $email],
                 [
                     'name' => $name,
-                    'google_id' => $id
+                    'google_id' => $id,
+                    'avatar' => $avatar_file
                 ]
             );
+            Auth::login($user);
             return redirect()->to('dashboard');
         } else {
             return redirect()->to('auth')->with('error', 'Akun yang anda gunakan tidak diizinkan untuk masuk halaman admin');
         }
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->to('auth');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\authController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\authController;
 use Laravel\Socialite\Facades\Socialite;
 
 /*
@@ -19,11 +20,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::redirect('home', 'dashboard');
 
-Route::get('/auth', [authController::class, "index"]);
-Route::get('/auth/redirect', [authController::class, "redirect"]);
-Route::get('/auth/callback', [authController::class, "callback"]);
+Route::get('/auth', [authController::class, "index"])->name('login')->middleware('guest');
+Route::get('/auth/redirect', [authController::class, "redirect"])->middleware('guest');
+Route::get('/auth/callback', [authController::class, "callback"])->middleware('guest');
+Route::get('/auth/logout', [authController::class, "logout"]);
 
 Route::get('/dashboard', function() {
-    return 'Selamat datang di halaman dashboard!';
-});
+    return view('dashboard.index');
+})->middleware('auth');
