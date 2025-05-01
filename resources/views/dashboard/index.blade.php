f1f017c58b4ca7ecb177f194bc6b7a0d08dfb2dd
Inov Afani <inoveka@gmail.com>
2025-05-01 23:06:25 +0700

    Update portofolio terbaru

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -35,7 +35,7 @@
         <ul class="navbar-nav navbar-nav-right">
           <li class="nav-item nav-profile dropdown">
             <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" id="profileDropdown">
-              <img src="{{ asset('admin') }}/images/faces/face5.jpg" alt="profile"/>
+              <img src="{{ asset('admin') }}/images/faces/{{Auth::user()->avatar}}" alt="profile"/>
               <span class="nav-profile-name">{{ Auth::user()->name }}</span>
             </a>
             <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
