f195128407449189f5c27245c7b9f6b91dccee8f
Inov Afani <inoveka@gmail.com>
2025-05-05 23:48:30 +0700

    Adding some controllers needs

--- app/Http/Controllers/halamanController.php
+++ app/Http/Controllers/halamanController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\halaman;
 use Illuminate\Http\Request;
 
 class halamanController extends Controller
@@ -34,7 +35,22 @@ class halamanController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate(
+            [
+                'judul'=> 'required',
+                'isi'=> 'required',
+            ],
+            [
+                'judul.required'=> 'Judul wajib diisi',
+                'isi.required'=> 'Isian tulisan wajib diisi',
+            ]
+            );
+        
+            $data = [
+                'judul'=>$request->judul,
+                'isi'=>$request->isi,
+            ];
+            halaman::create();
     }
 
     /**
--- resources/views/dashboard/halaman/create.blade.php
+++ resources/views/dashboard/halaman/create.blade.php
@@ -1,8 +1,8 @@
 @extends('dashboard.layout')
 @section('konten')
     <div class="pb-3">
-        <a href="{{route('halaman.index')}}">
-            << kembali
+        <a href="{{route('halaman.index')}}" class="btn btn-secondary">
+            Kembali
         </a>
     </div>
     <form action="{{route('halaman.store')}}" method="POST">
@@ -10,13 +10,17 @@
             <label for="judul" class="form-label">Judul</label>
             <input
                 type="text"
-                class="form-control"
+                class="form-control form-control-sm"
                 name="judul"
                 id="judul"
                 aria-describedby="helpId"
-                placeholder="Judul"
             />
         </div>
+        <div class="mb-3">
+            <label for="isi" class="form-label">Isi</label>
+            <textarea class="form-control" id="isi" name="isi" rows="5"></textarea>
+        </div>
+        <button class="btn btn-primary" name="simpan" type="submit">Simpan</button>
         
     </form>
 @endsection
