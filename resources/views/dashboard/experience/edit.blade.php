5306829514915fcd54cac667c4ea4a2316b39f7e
Inov Afani <inoveka@gmail.com>
2025-05-11 23:56:49 +0700

    Fix the error in edit

--- resources/views/dashboard/experience/edit.blade.php
+++ resources/views/dashboard/experience/edit.blade.php
@@ -42,7 +42,7 @@
         </div>
         <div class="mb-3">
             <label for="isi" class="form-label">Isi</label>
-            <textarea class="form-control summernote" id="isi" name="isi" rows="5" value="{{$data->isi}}"></textarea>
+            <textarea class="form-control summernote" id="isi" name="isi" rows="5">{{$data->isi}}</textarea>
         </div>
         <button class="btn btn-primary" name="simpan" type="submit">Simpan</button>
         
