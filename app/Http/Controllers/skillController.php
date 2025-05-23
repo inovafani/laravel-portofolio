3455cca0fbb4ee64d2630b7a6250268f7b948b8a
Inov Afani <inoveka@gmail.com>
2025-05-23 13:54:47 +0700

    Newest update

--- app/Http/Controllers/skillController.php
+++ app/Http/Controllers/skillController.php
@@ -9,7 +9,12 @@ class skillController extends Controller
 {
     public function index()
     {
-        return view('dashboard.skill.index');
+        $skill_url = public_path('admin/devicon.json');
+        $skill_data = file_get_contents($skill_url);
+        $skill_data = json_decode($skill_data, true);
+        $skill = array_column($skill_data, 'name');
+        $skill = implode("','", $skill) . "'";
+        return view('dashboard.skill.index')->with(['skill' => $skill]);
     }
 
     public function update(Request $request)
