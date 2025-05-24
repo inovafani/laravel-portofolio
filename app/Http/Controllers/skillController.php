cfea1c447afe10bec108780b2bf0857bea48bde9
Inov Afani <inoveka@gmail.com>
2025-05-24 15:15:17 +0700

    Skill menu done

--- app/Http/Controllers/skillController.php
+++ app/Http/Controllers/skillController.php
@@ -13,7 +13,7 @@ class skillController extends Controller
         $skill_data = file_get_contents($skill_url);
         $skill_data = json_decode($skill_data, true);
         $skill = array_column($skill_data, 'name');
-        $skill = implode("','", $skill) . "'";
+        $skill = "'" . implode("','", $skill) . "'";
         return view('dashboard.skill.index')->with(['skill' => $skill]);
     }
 
--- resources/views/dashboard/skill/index.blade.php
+++ resources/views/dashboard/skill/index.blade.php
@@ -1,13 +1,13 @@
 @extends('dashboard.layout')
 @section('konten')
-
     <form action="{{route('skill.update')}}" method="POST">
+
         @csrf
         <div class="mb-3">
             <label for="judul" class="form-label">PROGRAMMING LANGUAGE & TOOLS</label>
             <input
                 type="text"
-                class="form-control form-control-sm"
+                class="form-control form-control-sm skill"
                 name="_language"
                 id="judul"
                 aria-describedby="helpId"
@@ -18,7 +18,21 @@
             <label for="isi" class="form-label">WORKFLOW</label>
             <textarea class="form-control summernote" id="isi" name="_workflow" rows="5">{{get_meta_value('_workflow')}}</textarea>
         </div>
-        <button class="btn btn-primary" name="simpan" type="submit">Simpan</button>
+        <button class="btn btn-primary" name="simpan" type="submit">Update</button>
         
     </form>
 @endsection
+
+@push('child-script')
+    <script>
+    $(document).ready(function() {
+        $('.skill').tokenfield({
+            autocomplete: {
+                source: [{!! $skill !!}],
+                delay: 100
+            },
+            showAutocompleteOnFocus: true
+        });
+    });
+  </script> 
+@endpush
